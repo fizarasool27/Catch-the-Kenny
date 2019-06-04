2324d3075d98f7bf6a2ec3fdd0e6078aa7eee206
fizarasool27 <fizafr2127@gmail.com>
2019-06-05 03:05:08 +0530

    Initial Commit

---
+++ Catch the Kenny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Catch the Kenny
+//
+//  Created by Fiza Rasool on 05/06/19.
+//  Copyright © 2019 Fiza Rasool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
